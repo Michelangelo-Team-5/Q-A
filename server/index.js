d9dc078e009df3ea144a8718f0d3b3ecd7f3c944
Bruno <bberastain@gmail.com>
2021-08-14 11:32:23 -0700

    posting to questions and answers endpoints

--- server/index.js
+++ server/index.js
@@ -12,18 +12,11 @@ app.use(express.json());
 // retrieve a list of questions for a particular product
 // REMOVE REPORTED QUESTIONS
 app.get('/qa/questions', async (req, res) => {
-  let {product_id, page, count} = req.query;
+  let {product_id, page = 1, count = 5} = req.query;
   if (!product_id) {
     res.status(400).send('Error: invalid product_id provided');
   }
-  // page default
-  if (!page) {
-    page = 1;
-  }
-  // count default
-  if (!count) {
-    count = 5;
-  }
+
   // USE PAGE AND COUNT CONDITIONALLY
 
   let results = await db.query(`SELECT id as question_id, body as question_body, date_written as question_date, asker_name, helpful as question_helpfulness, reported FROM questions WHERE product_id = ${product_id}`);
@@ -50,13 +43,8 @@ app.get('/qa/questions/:question_id', async (req, res) => {
   // ADD A 400 RESPONSE IF NO QUESTION_ID IS GIVEN
 
   // USE PAGE AND COUNT CONDITIONALLY
-  let {page, count} = req.query;
-  if (!page) {
-    page = 1;
-  }
-  if (!count) {
-    count = 5;
-  }
+  let {page = 1, count = 5} = req.query;
+
   let question = req.params.question_id;
   let response = {question, page, count}
   let results = await db.query(`SELECT id as answer_id, body, date_written as date, answerer_name, helpful as helpfulness FROM answers WHERE question_id=${question}`);
@@ -68,33 +56,50 @@ app.get('/qa/questions/:question_id', async (req, res) => {
   res.status(200).send(response);
 });
 
-
-
-
 // add a question for the given product
-app.post('/qa/questions', (req, res) => {
+app.post('/qa/questions', async (req, res) => {
   let {body, name, email, product_id} = req.body;
   if (!body || !name || !email || !product_id) {
     res.status(400).send('Error: Question body contains invalid entries')
   }
-  // db.none(`INSERT INTO questions(product_id, body, asker_name, asker_email) VALUES($1, $2, $3, $4)`, [product_id, body, name, email])
-  //   .then(() => {
-  //     res.status(200).send('Created');
-  //   })
-  //   .catch(err => {
-  //     console.log(error);
-  //     res.send('404');
-  //   })
+  let id = await db.query(`SELECT MAX(id) from questions`);
+  debugger;
+  db.none(`INSERT INTO questions(id, product_id, body, date_written, asker_name, asker_email, reported) VALUES($1, $2, $3, $4, $5, $6, $7)`, [id[0].max + 1, product_id, body, Date.now(), name, email, false])
+    .then(() => {
+      res.status(201).send('Created');
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(404);
+    })
 });
 
-
-
-
 // add an answer for the given question
-app.post('/qa/questions/:question_id/answers', (req, res) => {
-  // question_id (parameter)
-  // body, name, email, photos
-  res.status(200).send('resopnse from api');
+app.post('/qa/questions/:question_id/answers', async (req, res) => {
+  let question_id = req.params.question_id;
+  let {body, name, email, photos = []} = req.body;
+  if (!body || !name || !email) {
+    res.status(400).send('Error: Answer body contains invalid entries')
+  }
+  let id = await db.query(`SELECT MAX(id) from answers`);
+  let answer_id = id[0].max + 1
+  db.none(`INSERT INTO answers(id, question_id, body, date_written, answerer_name, answerer_email, reported) VALUES($1, $2, $3, $4, $5, $6, $7)`, [answer_id, question_id, body, Date.now(), name, email, false])
+    .then(async () => {
+      for (let photo in photos) {
+        let photo_id = await db.query(`SELECT MAX(id) from answers_photos`);
+        db.none(`INSERT INTO answers_photos(id, answer_id, url) VALUES($1, $2, $3)`, [photo_id[0].max + 1, answer_id, photo])
+          .then(() => {})
+          .catch(err => {
+            console.log(err);
+            res.sendStatus(404);
+          });
+      }
+      res.status(201).send('Created');
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(404);
+    })
 });
 
 app.listen(port, () => {console.log(`Listening at http://localhost:${port}`)})
